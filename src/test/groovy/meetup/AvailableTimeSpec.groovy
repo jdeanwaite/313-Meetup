c3dfbbeb225f4448480014b880ac40c58c0106f0
Justin <jdeanwaite@gmail.com>
2016-12-10 18:59:22 -0700

    preparing for deployement

---
+++ src/test/groovy/meetup/AvailableTimeSpec.groovy
@@ -0,0 +1,22 @@
+package meetup
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(AvailableTime)
+class AvailableTimeSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
